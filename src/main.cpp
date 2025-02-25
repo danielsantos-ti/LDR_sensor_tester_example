d5da72c79493ade4d5a80191369d2cb0c4f4a707
danielsantos-ti <sysdsantos@gmail.com>
2025-02-25 11:14:41 -0300

    Atualiza configurações do monitor serial e refatora código no main.cpp para leitura do sensor LDR

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
 #include <Arduino.h>
 
 #define relePin 3
-#define sensorPin_LDR A5
+#define sensorLDR_pin A5
 
 
 void setup() {
   
-  
-  
+  pinMode(relePin, OUTPUT);
+  pinMode(sensorLDR_pin, INPUT);
+  Serial.begin(9600);
+  Serial.println("Sistema iniciado");
+
 }
 
 void loop() {
   
+  int valorDoSensor = analogRead(sensorLDR_pin);
+
+    if (valorDoSensor > 500){
+    digitalWrite(relePin, HIGH);
+  }else{
+    digitalWrite(relePin, LOW);
+  }
+
+  Serial.println(valorDoSensor);
+  delay(1000);
+
 }
 
