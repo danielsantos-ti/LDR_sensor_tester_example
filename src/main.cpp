d83654e5724e8e791e225b6b2f7ea12b91eb029b
danielsantos-ti <sysdsantos@gmail.com>
2025-02-26 16:00:41 -0300

    Adiciona comentários e estrutura ao código para controle de luminosidade com Arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
+// Author: Daniel Santos
+// Projeto: Controle de Luminosidade com Arduino
+// Adicionado: 2021-09-26
+// Last update: 2021-09-26 
+// Version: 0.1
+// Description: Controle de Luminosidade com Arduino
+
+// Bibliotecas
 #include <Arduino.h>
 
+// Definições dos pinos
 #define relePin 3
 #define sensorLDR_pin A5
 
-
+// Configuração dos pinos
 void setup() {
   
+  // Configuração dos pinos
   pinMode(relePin, OUTPUT);
   pinMode(sensorLDR_pin, INPUT);
   Serial.begin(9600);
@@ -13,16 +23,20 @@ void setup() {
 
 }
 
+// Loop principal
 void loop() {
   
+  // Leitura do sensor
   int valorDoSensor = analogRead(sensorLDR_pin);
 
-    if (valorDoSensor > 500){
+  // Controle do rele
+  if (valorDoSensor > 500){
     digitalWrite(relePin, HIGH);
   }else{
     digitalWrite(relePin, LOW);
   }
 
+  // Exibição do valor do sensor
   Serial.println(valorDoSensor);
   delay(1000);
 
